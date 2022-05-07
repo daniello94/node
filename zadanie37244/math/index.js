811713da53704e98b5c1c1116f23758873882003
daniello94 <97047890+daniello94@users.noreply.github.com>
2022-05-08 01:54:02 +0200

    zadanka

---
+++ zadanie37244/math/index.js
@@ -0,0 +1,8 @@
+const randomNumber = require('./random-number')
+const randomCode = require('./random-code')
+const randomArray = require('./random-array')
+module.exports = {
+    randomNumber: randomNumber,
+    randomCode: randomCode,
+    randomArray: randomArray
+}
---
+++ zadanie37244/math/random-number.js
@@ -0,0 +1,10 @@
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+module.exports = randomNumber
+// console.log(randomNumber(10,200));
+
+
+
